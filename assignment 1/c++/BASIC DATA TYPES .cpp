8d043649cf02afd1e624da2470b10cbac472d320
vishalkumarsing <106993591+vishalkumarsing@users.noreply.github.com>
2022-09-21 16:26:55 +0530

    Create BASIC DATA TYPES .cpp

---
+++ assignment 1/c++/BASIC DATA TYPES .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    int x;
+    long y;
+    char z;
+    float t;
+    double u;
+    
+    
+    scanf("%d %ld %c %f %lf",&x,&y,&z,&t,&u);
+    printf("%d\n%ld\n%c\n%f\n%lf",x,y,z,t,u);
+    
+    return 0;
+}
